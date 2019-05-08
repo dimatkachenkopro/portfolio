6cf080faa0add0dfab1d8321608b7dbbd06b1b94
Dima Tkachenko <dimatkachenkopro@gmail.com>
2019-05-08 21:05:22 +0200

    Updates

---
+++ precache-manifest.fff86509de64df650399e414fbd0fe8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61446511ad5d4f41e99003f5eadb5ab",
+    "url": "/dimatkachenkopro/portfolio/index.html"
+  },
+  {
+    "revision": "d0b0f4e7991424d1fc89",
+    "url": "/dimatkachenkopro/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "1c8e0fa18766fc656772",
+    "url": "/dimatkachenkopro/portfolio/static/css/main.ef9c2b25.chunk.css"
+  },
+  {
+    "revision": "d0b0f4e7991424d1fc89",
+    "url": "/dimatkachenkopro/portfolio/static/js/2.9108dbf8.chunk.js"
+  },
+  {
+    "revision": "1c8e0fa18766fc656772",
+    "url": "/dimatkachenkopro/portfolio/static/js/main.94f98dee.chunk.js"
+  },
+  {
+    "revision": "a7d618571242e81d11de",
+    "url": "/dimatkachenkopro/portfolio/static/js/runtime~main.c88a0ca2.js"
+  }
+]);
